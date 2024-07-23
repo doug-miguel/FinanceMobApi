1421232d51a35ddcb4a462239472c5beebea72ca
Douglas Miguel <doug_miguel@outlook.com>
2024-07-23 19:59:59 -0300

    fix-Token

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -6,13 +6,6 @@ import { authSchema } from '../models/auth.model.js';
 async function authRouter(fastify: FastifyInstance) {
     fastify.withTypeProvider<ZodTypeProvider>().post("", authSchema, Auth);
 
-    // fastify.get('/', async (req: FastifyRequest, res: FastifyReply) => {
-    //     return 'authRouter atualizar!';
-    // });
-
-    // fastify.post('/login', async (req: FastifyRequest, res: FastifyReply) => {
-    //     return 'authRouter login!';
-    // });
 }
 
 export default authRouter;
