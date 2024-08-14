c59395bc27cd3e73476ba97b8c2bca81ea12d18a
Douglas Miguel <doug_miguel@outlook.com>
2024-08-13 22:27:17 -0300

    Corrigir configuração de saída para Vercel

--- src/index.ts
+++ src/index.ts
@@ -4,7 +4,6 @@ const port: number = parseInt(process.env.PORT || "8080");
 const start = async () => {
   try {
     await app.listen({ port: port });
-    console.log(`Server is running on port ${port}`);
   } catch (err) {
     console.error(err);
     process.exit(1);
