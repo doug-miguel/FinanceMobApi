d01d51d777528fff4f6436a2dd6f57f70008c9f9
Douglas Miguel <doug_miguel@outlook.com>
2024-08-14 15:16:01 -0300

    teste

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import { app } from "./server.js";
 const port: number = parseInt(process.env.PORT || "8080");
 
+console.log(`Starting server on port ${port}`);
+
 const start = async () => {
   try {
     await app.listen({ port: port });
+    console.log(`Server started on port ${port}`);
   } catch (err) {
-    console.error(err);
+    console.error("Error starting server:", err);
     process.exit(1);
   }
 };
