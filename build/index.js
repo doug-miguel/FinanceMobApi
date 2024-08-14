084cc9ea6e74700987b3dcf64232f3df9d4a6ff6
Douglas Miguel <doug_miguel@outlook.com>
2024-08-13 22:27:53 -0300

    fix

--- build/index.js
+++ build/index.js
@@ -5,7 +5,6 @@ const port = parseInt(process.env.PORT || "8080");
 const start = async () => {
     try {
         await server_js_1.app.listen({ port: port });
-        console.log(`Server is running on port ${port}`);
     }
     catch (err) {
         console.error(err);
