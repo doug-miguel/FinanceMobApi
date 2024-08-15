41ec3582a4319ff806100d9dfd6ab6eb542b7c46
Douglas Miguel <doug_miguel@outlook.com>
2024-08-14 22:17:48 -0300

    clear console

--- src/index.ts
+++ src/index.ts
@@ -6,9 +6,7 @@ console.log(`Starting server on port ${port}`);
 const start = async () => {
   try {
     await app.listen({ port: port });
-    console.log(`Server started on port ${port}`);
   } catch (err) {
-    console.error("Error starting server:", err);
     process.exit(1);
   }
 };
